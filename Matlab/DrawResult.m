0e8a3d13d6f598caa317fd99d7b3a1b2f1e6cc8b
kylewdonaldson <62639807+kylewdonaldson@users.noreply.github.com>
2020-04-15 23:13:25 +0200

    Work on Draw result function, implement correct coefficients in SetUp and add get color function for color mapping.

--- Matlab/DrawResult.m
+++ Matlab/DrawResult.m
@@ -5,22 +5,65 @@
 
 % Define base colour (grey) 
 cbase = [0.5 0.5 0.5];
+% Define air colour 
+cair = [0 1 1];
+% Define water color
+cwater = [0 0 1];
 
 % create the plotting area, define limits, set aspect ratio 
-clf
+
 figure
 hold on 
-xlim ([0 40])
-ylim ([0 40])
+xlim ([0 20])
+ylim ([0 20])
 pbaspect([1 1 1])
 
 %% Define the corner points 
-mincorner = [4,4];
-maxcorner = [16,28];
+% for the world
+mincorner = [0,0];
+maxcorner = [5,9];
 
-%% draw the rectangles 
-for i = mincorner(1):maxcorner(1) 
+% for the metal block 
+blockmincorner = [1,1];
+blockmaxcorner = [4,8];
+
+%% Draw all the lower blocks as water color rectangles 
+for i = mincorner(1):maxcorner(1)-1 
    for j = mincorner(2):maxcorner(2)
-        rectangle('Position',[j,i,0.5,0.5],'FaceColor',cbase,'EdgeColor','k','LineWidth',1)
+        rectangle('Position',[j,i,0.8,0.8],'FaceColor',cwater,'EdgeColor','k','LineWidth',1)
    end
 end
+
+% Draw the row of air blocks
+for i = 0:maxcorner(2)
+    rectangle('Position',[i,5,0.8,0.8],'FaceColor',cair,'EdgeColor','k','LineWidth',1)
+end
+
+% Draw the metal block 
+for i = blockmincorner(1):blockmaxcorner(1) 
+   for j = blockmincorner(2):blockmaxcorner(2)
+        rectangle('Position',[j,i,0.8,0.8],'FaceColor',cbase,'EdgeColor','k','LineWidth',1)
+   end
+end
+
+%% we must associate each rectangle of the metal block with a node..
+% (this a terrible hack) Define a nodal mapping matrix
+nodalmapping = [7 8 8 9 9 8 8 7;
+                4 5 5 6 6 5 5 4;
+                4 5 5 6 6 5 5 4;
+                1 2 2 3 3 2 2 1];
+%% Define a color map 
+cmap = cool(65);
+
+%% 
+% for each time in the temp history matrix 
+for t=1:size(NodalTempHistory,2)
+    % Update the drawing of the metal block 
+    for i = blockmincorner(1):blockmaxcorner(1) 
+       for j = blockmincorner(2):blockmaxcorner(2)
+            rectangle('Position',[j,i,0.8,0.8],'FaceColor',getcolor(cmap,NodalTempHistory(nodalmapping(i,j),t)),'EdgeColor','k','LineWidth',1)
+       end
+    end
+    pause(0.05);
+end
+
---
+++ Matlab/getcolor.m
@@ -0,0 +1,12 @@
+%% Get the closest entry out of the colormap for the current temp 
+function color = getcolor(cmap,temp)
+    % scale the temperature to a value between 0 and 100 for the range 700
+    % to 800
+
+    for i=1:size(cmap,1)
+       if(i>=(temp-750))
+           color = cmap(i,:);
+           break;
+       end
+    end
+end
